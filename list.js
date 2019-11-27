25e0085fef14fa50dd8aced8a661145fbb40ab1b
john1eng <john1eng@hotmail.com>
2019-11-27 15:28:44 -0500

    delete prompt in list.js

--- list.js
+++ list.js
@@ -4,29 +4,10 @@ var subject;
 //get data
 
 
-
-
 window.addEventListener("load", function(){
 //get data
 $.get("https://jsonstorage.net/api/items/22738f45-0838-4697-b7a6-f5d0dc99acd6",
 function(data, textStatus, jqXHR){
-subject = prompt("What is the subject?");
-
-//add to data[]
-data.push({'key':subject});
-var newDataJ = JSON.stringify(data);
-
-//update data
-$.ajax({
-    url:"https://jsonstorage.net/api/items/22738f45-0838-4697-b7a6-f5d0dc99acd6",
-    type:"PUT",
-    data:newDataJ,
-    contentType:"application/json; charset=utf-8",
-    dataType:"json",
-    success: function(data, textStatus, jqXHR){
-
-    }
-}); //end update data
 
 var index = 0;
 
