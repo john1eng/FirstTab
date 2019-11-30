9ebba506a19428fdf296519c1824886f34810124
john1eng <john1eng@hotmail.com>
2019-11-30 10:49:03 -0500

    add clear button in list.html

--- list.js
+++ list.js
@@ -24,3 +24,18 @@ pull_data.insertAdjacentHTML('afterbegin',htmlString);
 
 }) //end of get data and this way the data[] can be access
 }) //addEventListener ended
+
+$(document).ready(function() {
+$("#clear").click(function() {
+  $.ajax({
+      url:"https://jsonstorage.net/api/items/22738f45-0838-4697-b7a6-f5d0dc99acd6",
+      type:"PUT",
+      data:'[]',
+      contentType:"application/json; charset=utf-8",
+      dataType:"json",
+      success: function(data, textStatus, jqXHR){
+
+      }
+  });
+})
+})
